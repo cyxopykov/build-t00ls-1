2b18ab4f8c4cdd316692a5de75558d0c92da6e76
Anatol Sukharukau <80684660+cyxopykov@users.noreply.github.com>
2022-03-23 10:45:32 +0300

    Update HelloWorldService.java

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldService.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldService.java
@@ -18,7 +18,7 @@ package org.jboss.as.quickstarts.wshelloworld;
 
 import java.util.List;
 import javax.jws.WebMethod;
-import javax.jws.WebService;
+import javax.jws.WebService.*;
 
 /**
  * A simple example of how to setup a JAX-WS Web Service. It can say hello to everyone or to someone in particular.
